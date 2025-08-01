0a294e6d9f45ff15fadfa1d91f027bbb0cd04e10
branles14 <branles@pm.me>
2025-07-31 21:03:57 -0600

    removed python refactor of original zsh script

--- weather.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Entry point for the :mod:`weather` command line interface."""
-
-from weather.cli import main
-
-
-if __name__ == "__main__":  # pragma: no cover - script entry
-    main()
